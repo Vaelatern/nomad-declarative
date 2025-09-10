36b28e791347c100a4d97ff82b64d4d156f804be
Toyam Cox <Vaelatern@voidlinux.org>
2025-09-10 01:34:55 -0400

    Problem: Doesn't compile
    Solution: Correct the variable name

--- cmd/nomad-declarative/main.go
+++ cmd/nomad-declarative/main.go
@@ -46,7 +46,7 @@ func ParseJob(job confparse.Job, root fs.FS, fileWrite func(string, []byte) erro
 		origin = job.Pack["origin"].(string)
 	}
 
-	if strings.HasPrefix(origin, "./") || !strings.Contains(path, "://") {
+	if strings.HasPrefix(origin, "./") || !strings.Contains(origin, "://") {
 		cwd, err := os.Getwd()
 		if err == nil {
 			origin = "file://" + cwd + "/" + origin
